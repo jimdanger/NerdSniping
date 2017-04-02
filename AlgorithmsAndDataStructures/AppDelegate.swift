f22cd448e4f02ad3b528e0f7a2fb04de4760b011
Jim Wilson <jamesmattheywilson@gmail.com>
2017-04-02 11:31:55 -0400

    Initial Commit

---
+++ AlgorithmsAndDataStructures/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Jim Wilson on 4/2/17.
+//  Copyright © 2017 Jim Danger, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
