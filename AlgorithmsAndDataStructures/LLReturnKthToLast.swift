a1f1d0b90d87f252ab9722bbc12cd994e6441d37
Jim Wilson <jamesmattheywilson@gmail.com>
2017-06-14 09:04:22 -0400

    WIP: kth to last. whiteboarding solutions
    had to run to work. will implement later. Sorry for the crappy commit message, but this commit only commits comments, which will be deleted later.

--- AlgorithmsAndDataStructures/LLReturnKthToLast.swift
+++ AlgorithmsAndDataStructures/LLReturnKthToLast.swift
@@ -23,6 +23,27 @@ class LLReturnKthToLast: LinkedList {
     
     func getKthToLast(k: Int) -> LinkedListNode {
         
+        /* Whiteboard:
+            - iterate through once to count, and again to find Kth to last. O(N^2)
+        
+            - iterate a few times, once with a runner that skips a 1000 or so,
+            then again skipping 1000/2, etc. that way we could find the last one 
+            a lot quicker
+                - how does this even work? I guess I just loop 1000 times always advanceing to the next node without actually performaing a computation on the node
+                - so for this problem (where there is no computation on the node anyway), i don't quite see how this saves computational time. It may even add time.
+                -  is node.next.next the same computational speed as doing node.next twice?
+         
+            - Can we do better?
+         
+            - hmmm...
+         
+            - As I advance through the LL, I could keep track of the node that is k behind the current position. Then, when I get to the end, we can instantly return that node.
+                - This one is the winner. this is O(N), might be hard to do better.
+         
+        
+        */
+        
+        
         
     }
     
