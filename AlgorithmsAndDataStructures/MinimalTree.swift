a2e3be8633080cf6d62fb1211079f4c844c595d8
Jim Wilson <jamesmattheywilson@gmail.com>
2017-07-13 08:39:05 -0400

    Can create a minimal height tree from sorted array
    Minimal Tree: given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.

---
+++ AlgorithmsAndDataStructures/MinimalTree.swift
@@ -0,0 +1,57 @@
+//
+//  MinimalTree.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Jim Wilson on 7/13/17.
+//  Copyright © 2017 Jim Danger, LLC. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 
+ Crack the Coding Interview 4.2:
+ 
+ Minimal Tree: given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+ 
+ */
+
+
+class MinTreeNode {
+    var data: Int
+    var left: MinTreeNode?
+    var right: MinTreeNode?
+    init (data: Int ){
+        self.data  = data
+    }
+}
+
+class MinimalTree {
+    
+    var root: MinTreeNode?
+    
+    init(array: [Int]) {
+        self.root = create(array: array)
+    }
+    
+    func create(array: [Int]) -> MinTreeNode? {
+        if array.isEmpty {
+            return nil
+        }
+        
+        let middleIndex = findMiddleIndex(array: array)
+        let result = MinTreeNode(data: array[middleIndex])
+        
+        let leftArray = Array(array[0..<middleIndex])
+        let rightArray = Array(array[middleIndex..<array.count])
+        
+        result.left = create(array: leftArray)
+        result.right = create(array: rightArray)
+        
+        return result
+    }
+    
+    func findMiddleIndex(array: [Int]) -> Int {
+        return  array.count/2
+    } 
+}
