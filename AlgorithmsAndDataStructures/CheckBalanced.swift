474a8201aa38cc731349f8bf91471536c6c6b91e
Jim Wilson <jamesmattheywilson@gmail.com>
2017-07-16 12:50:37 -0400

    Check if a binary tree is balanced
    Implement a function to check if a binary tree is balanced.
    
    For the purposes of this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.

---
+++ AlgorithmsAndDataStructures/CheckBalanced.swift
@@ -0,0 +1,75 @@
+//
+//  CheckBalanced .swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Jim Wilson on 7/16/17.
+//  Copyright © 2017 Jim Danger, LLC. All rights reserved.
+//
+
+/**
+ 
+ * Crack the Coding Interview 4.4:
+ * Implement a function to check if a binary tree is balanced.
+ * For the purposes of this question, a balanced tree is defined
+ * to be a tree such that the heights of the two subtrees of any
+ * node never differ by more than one.
+ */
+
+
+import Foundation
+
+class CBNode {
+    var data: Int
+    var left: CBNode?
+    var right: CBNode?
+    
+    init(data: Int){
+        self.data = data
+        
+    }
+    
+}
+
+class CheckBalanced {
+    
+    
+    func isBalanced(root: CBNode) -> Bool {
+        if root.left == nil && root.right == nil {
+            return true
+        }
+       
+        var leftHeight = 1
+        if root.left == nil {
+            leftHeight = 0
+        } else {
+            leftHeight += height(node: root.left!)
+        }
+        
+        var rightHeight = 1
+        if root.right == nil {
+            rightHeight = 0
+        } else {
+            rightHeight += height(node: root.right!)
+        }        
+        return abs(rightHeight - leftHeight) <= 1
+        
+    }
+    
+    func height(node: CBNode) -> Int {
+        
+        if node.left == nil && node.right == nil {
+            return 0
+        }
+        
+        var leftHeight = 0
+        if node.left != nil{
+            leftHeight = height(node: node.left!) + 1
+        }
+        var rightHeight = 0
+        if node.right != nil{
+            rightHeight = height(node: node.right!) + 1
+        }
+        return leftHeight >= rightHeight ? leftHeight : rightHeight
+    }
+}
+
---
+++ AlgorithmsAndDataStructuresTests/Tests_CheckBalanced.swift
@@ -0,0 +1,62 @@
+//
+//  Tests_CheckBalanced.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Jim Wilson on 7/16/17.
+//  Copyright © 2017 Jim Danger, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import AlgorithmsAndDataStructures
+
+class Tests_CheckBalanced: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+
+    func test_height() {
+        let x = CheckBalanced()
+        let root = CBNode(data: 0)
+        XCTAssertEqual(x.height(node: root), 0)
+        
+        let l = CBNode(data: 1)
+        let r = CBNode(data: 2)
+        root.left = l
+        root.right = r
+        XCTAssertEqual(x.height(node: root), 1)
+    
+        let ll = CBNode(data: 3)
+        let rr = CBNode(data: 4)
+        l.left = ll
+        r.right = rr
+        XCTAssertEqual(x.height(node: root), 2)
+
+        let lll = CBNode(data: 5)
+        let rrr = CBNode(data: 6)
+        ll.left = lll
+        ll.right = rrr
+        XCTAssertEqual(x.height(node: root), 3)
+    }
+    
+    
+    func test_isBalanced() {
+        
+        let x = CheckBalanced()
+        let root = CBNode(data: 0)
+        let rootLeft = CBNode(data: 0)
+        root.left = rootLeft
+        let rootleftleft = CBNode(data: 0)
+        root.left?.left = rootleftleft
+        
+        XCTAssertFalse(x.isBalanced(root: root))
+       
+
+    }
+
+}
